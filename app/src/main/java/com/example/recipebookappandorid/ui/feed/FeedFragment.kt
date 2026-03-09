2eb2f5b7314cd1f7608d731c714bb8e211867fa2
hadardaniell <hadar808080@gmail.com>
2026-03-09 19:07:52 +0200

    checkout

--- app/src/main/java/com/example/recipebookappandorid/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/example/recipebookappandorid/ui/feed/FeedFragment.kt
@@ -1,6 +1,33 @@
 package com.example.recipebookappandorid.ui.feed
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.recipebookappandorid.R
+import com.example.recipebookappandorid.databinding.FragmentFeedBinding
+import com.example.recipebookappandorid.viewmodel.AuthViewModel
 
-class FeedFragment : Fragment(R.layout.fragment_feed)
+class FeedFragment : Fragment(R.layout.fragment_feed) {
+
+    private var _binding: FragmentFeedBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: AuthViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentFeedBinding.bind(view)
+
+        binding.btnLogout.setOnClickListener {
+            viewModel.logout()
+            findNavController().navigate(R.id.loginFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
