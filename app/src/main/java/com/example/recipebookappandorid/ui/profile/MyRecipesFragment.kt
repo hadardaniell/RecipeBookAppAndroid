b4b8b2f0ac3af5d9e0b01dfb029c01e5a088e9df
hadardaniell <hadar808080@gmail.com>
2026-04-29 23:19:43 +0300

    profile page

--- app/src/main/java/com/example/recipebookappandorid/ui/profile/MyRecipesFragment.kt
+++ app/src/main/java/com/example/recipebookappandorid/ui/profile/MyRecipesFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import com.example.recipebookappandorid.R
@@ -32,7 +33,7 @@ class MyRecipesFragment : Fragment(R.layout.fragment_my_recipes) {
         }
 
         recipesAdapter = MyRecipesAdapter(::openRecipe, ::confirmRemoveRecipe)
-        binding.rvMyRecipes.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvMyRecipes.layoutManager = GridLayoutManager(requireContext(), 2)
         binding.rvMyRecipes.adapter = recipesAdapter
 
         viewModel.myRecipes.observe(viewLifecycleOwner) { recipes ->
